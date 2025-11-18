d5eca98c9a677cdc54b47720e9def243725b1d71
Wayne Buchner <wayne.buchner@qantas.com.au>
2025-11-18 11:20:06 +1100

    New Flight Data Class

---
+++ SwiftCode_Challenge/SwiftCode_Challenge/SwiftCode_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCode_ChallengeApp.swift
+//  SwiftCode_Challenge
+//
+//  Created by Wayne Buchner on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCode_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftCode_Challenge/SwiftCode_ChallengeTests/SwiftCode_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCode_ChallengeTests.swift
+//  SwiftCode_ChallengeTests
+//
+//  Created by Wayne Buchner on 18/11/2025.
+//
+
+import Testing
+@testable import SwiftCode_Challenge
+
+struct SwiftCode_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
